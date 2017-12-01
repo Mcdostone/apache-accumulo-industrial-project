53c38afed2ba465af4d26c7ad90092ab65fefdf9
Yann Prono <yann.prono@telecomnancy.net>
2017-12-01 18:21:56 +0100

    fix

--- docker/accumulo/entrypoint.sh
+++ docker/accumulo/entrypoint.sh
@@ -4,6 +4,7 @@ sed -i "s/localhost:2181/$ZOOKEEPER_PORT_2181_TCP_ADDR:2181/g" ${ACCUMULO_HOME}/
 sed -i "s#<value></value>#<value>hdfs://localhost:9000/accumulo</value>#g" ${ACCUMULO_HOME}/conf/accumulo-site.xml
 sed -i "s/DEFAULT/$(tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1)/g" ${ACCUMULO_HOME}/conf/accumulo-site.xml
 
+${ACCUMULO_HOME}/bin/start-all.sh
 # Accumulo and hadoop separated
 # sed -i "s#<value></value>#<value>hdfs://$HADOOP_PORT_9000_TCP_ADDR:9000/accumulo</value>#g" ${ACCUMULO_HOME}/conf/accumulo-site.xml
 
