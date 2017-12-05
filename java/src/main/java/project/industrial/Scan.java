0ec6d2ffefa1cbb3b1e01d8eaf95423c70413c8d
pierremaeckereel <pierre.maeckereel@gmail.com>
2017-12-05 17:01:42 +0100

    ajout de la fonctionnalité scan partiel (par range de row_ID) dans la classe Scan

--- java/src/main/java/project/industrial/Scan.java
+++ java/src/main/java/project/industrial/Scan.java
@@ -24,25 +24,55 @@ import com.beust.jcommander.Parameter;
 
 public class Scan {
 
+	/**
+	 * Optional arguments for the Scan class
+	 */
+	public static class Opts extends ClientOnRequiredTable {
+	    @Parameter(names = "--min", description = "minimum row that will be scanned")
+	    String min = "";
+	    @Parameter(names = "--max", description = "maximum row that will be scanned")
+	    String max = "";
+	  }
 	
 	/**
-	   * Scans over a specified number of entries to Accumulo using a {@link BatchScanner}. Completes scans twice to compare times for a fresh query with those for
-	   * a repeated query which has cached metadata and connections already established.
+	   * Scans an Accumulo table using a {@link Scanner}.
+	   * Prints the raw_ID, column family, column qualifier, visibility and value.
+	   * Possibility to specify a range of raw_ID with options max and min
 	   */
 	  public static void main(String[] args) throws AccumuloException, AccumuloSecurityException, TableNotFoundException {
-		ClientOnRequiredTable opts = new ClientOnRequiredTable();
+		Opts opts = new Opts();
 	    ScannerOpts sOpts = new ScannerOpts();
 	    opts.parseArgs(Scan.class.getName(), args, sOpts);
 
 	    Connector connector = opts.getConnector();
 	    Scanner scanner = connector.createScanner(opts.getTableName(), opts.auths);
 
-	    System.out.println("Scanning " + opts.getTableName());
+	    /*
+	     *  By default, the range is (-Inf, +Inf)
+	     *  So there is nothing to change for a full scan
+	     */
 	    
-	    System.out.println("Results ->");
-	    for (Entry<Key, Value> entry : scanner) {
-	    System.out.println(" " + entry.getKey() + " " + entry.getValue());
+	    System.out.println("Scanning " + opts.getTableName());
+	    if (!opts.min.equals("")) {
+	    	if (!opts.max.equals("")) {
+	    		scanner.setRange(new Range(opts.min, opts.max));
+	    		System.out.println("Scanning from row : " + opts.min + " to row : " + opts.max);
+	    	}
+	    	else {
+	    		scanner.setRange(new Range(opts.min, null));
+	    		System.out.println("Scanning from row : " + opts.min);
+	    	}
 	    }
+	    else {
+	    	if (!opts.max.equals("")) {
+	    		scanner.setRange(new Range(null,opts.max));
+	    		System.out.println("scanning to row " + opts.max);
+	    	}
+	    }
+	    
+	    // Printing all the scanned rows
+	    System.out.println("Results ->");
+	    new Printer().printAll(scanner.iterator());
 	    
 	  }
 }
