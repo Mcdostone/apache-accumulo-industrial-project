8b1a6f2999c622cb0b52106d70e17a2ba04c6040
Yann Prono <yann.prono@telecomnancy.net>
2017-12-05 13:00:49 +0100

    Add new class for fetching only one row

--- java/src/main/java/project/industrial/Scan.java
+++ java/src/main/java/project/industrial/Scan.java
@@ -1,30 +1,15 @@
 package project.industrial;
 
-import java.util.Map.Entry;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.accumulo.core.cli.BatchScannerOpts;
 import org.apache.accumulo.core.cli.ClientOnRequiredTable;
 import org.apache.accumulo.core.cli.ScannerOpts;
-import org.apache.accumulo.core.client.AccumuloException;
-import org.apache.accumulo.core.client.AccumuloSecurityException;
-import org.apache.accumulo.core.client.BatchScanner;
-import org.apache.accumulo.core.client.Connector;
-import org.apache.accumulo.core.client.Scanner;
-import org.apache.accumulo.core.client.TableNotFoundException;
-import org.apache.accumulo.core.data.Key;
-import org.apache.accumulo.core.data.Range;
-import org.apache.accumulo.core.data.Value;
-import org.apache.accumulo.core.security.Authorizations;
-
-import com.beust.jcommander.Parameter;
+import org.apache.accumulo.core.client.*;
+import org.apache.log4j.Logger;
 
 public class Scan {
 
-	
+	private static Logger logger = Logger.getLogger(Scan.class);
+
+
 	/**
 	   * Scans over a specified number of entries to Accumulo using a {@link BatchScanner}. Completes scans twice to compare times for a fresh query with those for
 	   * a repeated query which has cached metadata and connections already established.
@@ -37,12 +22,8 @@ public class Scan {
 	    Connector connector = opts.getConnector();
 	    Scanner scanner = connector.createScanner(opts.getTableName(), opts.auths);
 
-	    System.out.println("Scanning " + opts.getTableName());
-	    
-	    System.out.println("Results ->");
-	    for (Entry<Key, Value> entry : scanner) {
-	    System.out.println(" " + entry.getKey() + " " + entry.getValue());
-	    }
-	    
+	    logger.info("Scanning " + opts.getTableName() + "\n");
+
+	    Printer.printAll(scanner.iterator());
 	  }
 }
