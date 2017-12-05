5ec4a7b66449788d4537d62b3d73c4b4845ada24
Yann Prono <yann.prono@telecomnancy.net>
2017-12-05 17:02:59 +0100

    Create People Injector

---
+++ java/src/main/java/project/industrial/PeopleInjector.java
@@ -0,0 +1,93 @@
+package project.industrial;
+
+import org.apache.accumulo.core.cli.BatchWriterOpts;
+import org.apache.accumulo.core.cli.ClientOnRequiredTable;
+import org.apache.accumulo.core.client.BatchWriter;
+import org.apache.accumulo.core.client.Connector;
+import org.apache.accumulo.core.client.MutationsRejectedException;
+import org.apache.accumulo.core.data.Mutation;
+import org.apache.accumulo.core.security.ColumnVisibility;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class that injects some data into accumulo
+ * @author Yann Prono
+ */
+public class PeopleInjector {
+
+    private static final String FILENAME = "people.tsv";
+    private static final Logger logger = LoggerFactory.getLogger(PeopleInjector.class);
+    private BatchWriter bw;
+
+    public PeopleInjector(BatchWriter bw) {
+        this.bw = bw;
+    }
+
+    /**
+     * Read all file and insert data
+     */
+    public void insert() {
+        InputStream in = PeopleInjector.class.getResourceAsStream('/' + FILENAME);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        String line;
+        int countLine = 1;
+        try {
+            while((line = reader.readLine()) != null) {
+                reader.readLine();
+                String[] data = line.split("\t");
+                this.bw.addMutations(createMutations(countLine, data));
+                countLine++;
+            }
+            this.bw.close();
+        } catch (MutationsRejectedException | IOException e1) {
+            e1.printStackTrace();
+        }
+        logger.info(countLine - 1 + " rows has been inserted");
+    }
+
+    /**
+     * @return Mutations from the person data
+     */
+    private List<Mutation> createMutations(int id, String[] data) {
+        List<Mutation> mutations = new ArrayList<>();
+        Mutation name = new Mutation(String.valueOf(id));
+        name.put(
+                "identity",
+                "name",
+                new ColumnVisibility(""),
+                data[1]
+        );
+        Mutation city = new Mutation(String.valueOf(id));
+        city.put(
+                "identity",
+                "city",
+                new ColumnVisibility(""),
+                data[3]
+        );
+        mutations.add(name);
+        mutations.add(city);
+        return mutations;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Init the connection with accumulo
+        ClientOnRequiredTable opts = new ClientOnRequiredTable();
+        BatchWriterOpts bwOpts = new BatchWriterOpts();
+        opts.parseArgs(PartitioningBatchWriter.class.getName(), args, bwOpts);
+        Connector connector = opts.getConnector();
+        if(!connector.tableOperations().exists(opts.getTableName()))
+            connector.tableOperations().create(opts.getTableName());
+        BatchWriter bw = connector.createBatchWriter(opts.getTableName(), bwOpts.getBatchWriterConfig());
+        PeopleInjector injector = new PeopleInjector(bw);
+        injector.insert();
+    }
+
+}
---
+++ java/src/main/java/project/industrial/mining/GetByRange.java
@@ -0,0 +1,47 @@
+package project.industrial.mining;
+
+import com.beust.jcommander.Parameter;
+import org.apache.accumulo.core.cli.ClientOnRequiredTable;
+import org.apache.accumulo.core.cli.ScannerOpts;
+import org.apache.accumulo.core.client.*;
+import org.apache.accumulo.core.data.Range;
+import org.apache.hadoop.io.Text;
+import org.apache.log4j.Logger;
+import project.industrial.Printer;
+
+/**
+ * Fetch all rows that belongs to the range
+ * @author Yann Prono
+ */
+public class GetByRange {
+
+    private static Logger logger = Logger.getLogger(GetByKey.class);
+
+    static class Opts extends ClientOnRequiredTable {
+        @Parameter(names = "--start", required = true, description = "the beginning of range")
+        String start = null;
+        @Parameter(names = "--end", required = true, description = "the end of range")
+        String end = null;
+        @Parameter(names = "--filter", description = "Filter by column name cf:cq")
+        String filter = null;
+    }
+
+    public static void main(String[] args) throws AccumuloException, AccumuloSecurityException, TableNotFoundException {
+        GetByRange.Opts opts = new GetByRange.Opts();
+        ScannerOpts sOpts = new ScannerOpts();
+        opts.parseArgs(GetByKey.class.getName(), args, sOpts);
+        Connector connector = opts.getConnector();
+        if(!connector.tableOperations().exists(opts.getTableName()))
+            logger.warn("Table " + opts.getTableName() + " doesn't exist");
+        else {
+            Scanner scanner = connector.createScanner(opts.getTableName(), opts.auths);
+            scanner.setRange(new Range(opts.start, opts.end));
+            if(opts.filter != null)
+                scanner.fetchColumn(
+                        new Text(opts.filter.split(":")[0]),
+                        new Text(opts.filter.split(":")[1])
+                );
+            Printer.printAll(scanner.iterator());
+        }
+    }
+}
