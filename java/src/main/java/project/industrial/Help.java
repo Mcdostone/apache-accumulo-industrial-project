dfead6079b3d9548441e133c83ea37992dd5c775
samiabenjida <FldGib)ok,P>
2017-12-12 11:50:07 +0100

    add Twice

--- java/src/main/java/project/industrial/Help.java
+++ java/src/main/java/project/industrial/Help.java
@@ -36,6 +36,7 @@ public class Help {
         classes.add(GetByList.class);
         classes.add(GetByKey.class);
         classes.add(PeopleInjector.class);
+        classes.add(AddTwice.class);
         System.out.printf("%d java classes available:\n", classes.size());
         for(Class c: classes)
             System.out.printf("\t%s\n", c.getName());
