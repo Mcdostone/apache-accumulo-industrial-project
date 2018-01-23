bab5e6ec15dddc834d5b545428d7db62048d49c0
Yann Prono <yann.prono@telecomnancy.net>
2018-01-23 17:15:37 +0100

    add deleteTable class

---
+++ java/src/main/java/project/industrial/benchmark/main/DeleteTable.java
@@ -0,0 +1,34 @@
+package project.industrial.benchmark.main;
+
+import com.beust.jcommander.Parameter;
+import org.apache.accumulo.core.cli.ClientOpts;
+import org.apache.accumulo.core.client.Connector;
+import project.industrial.benchmark.core.Scenario;
+
+public class DeleteTable {
+
+
+    public static class TableOpts extends ClientOpts {
+        @Parameter(names = {"-t", "--table"}, description = "table to use")
+        private String tableName;
+
+        public String getTableName() {
+            return this.tableName;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        TableOpts opts = new TableOpts();
+        opts.parseArgs(DeleteTable.class.getName(), args);
+        Connector connector = opts.getConnector();
+        String tableName = opts.tableName;
+
+        if(tableName == null )
+            tableName = Scenario.askInput("Name of the table to delete:");
+
+        if(connector.tableOperations().exists(tableName))
+            connector.tableOperations().delete(tableName);
+        else
+            System.out.println(String.format("The table %s doesn't exist", tableName));
+    }
+}
