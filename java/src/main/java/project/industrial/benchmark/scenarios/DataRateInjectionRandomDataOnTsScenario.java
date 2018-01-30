48dcdb43fbd0edf88a5889fbf881b84269ac3aa6
Yann Prono <yann.prono@telecomnancy.net>
2018-01-30 11:43:04 +0100

    multiple clients with presplitting but force to hit one ts

--- java/src/main/java/project/industrial/benchmark/scenarios/DataRateInjectionRandomDataOnTsScenario.java
+++ java/src/main/java/project/industrial/benchmark/scenarios/DataRateInjectionRandomDataOnTsScenario.java
@@ -1,5 +1,6 @@
 package project.industrial.benchmark.scenarios;
 
+import com.beust.jcommander.Parameter;
 import com.codahale.metrics.Meter;
 import org.apache.accumulo.core.cli.BatchWriterOpts;
 import org.apache.accumulo.core.cli.ClientOnRequiredTable;
@@ -11,56 +12,37 @@ import project.industrial.benchmark.core.Scenario;
 
 import java.util.Random;
 
-public class DataRateInjectionRandomDataScenario extends Scenario {
-
-    protected final BatchWriter bw;
-    protected final Meter meter;
-
-    public DataRateInjectionRandomDataScenario(String cls, BatchWriter bw) {
-        super(cls);
-        this.bw = bw;
-        this.meter = MetricsManager.getMetricRegistry().meter("rate_injection");
-    }
-
-    public DataRateInjectionRandomDataScenario(BatchWriter bw) {
-        this(DataRateInjectionRandomDataScenario.class.getSimpleName(), bw);
-    }
-
-
-    @Override
-    protected void action() throws Exception {
-        int i = 0;
-        while(i <= 30000000) {
-            this.bw.addMutation(this.createRandomMutation(i));
-            this.meter.mark();
-            i++;
-        }
+public class DataRateInjectionRandomDataOnTsScenario extends DataRateInjectionRandomDataScenario {
+    private String rowKey;
+    public DataRateInjectionRandomDataOnTsScenario(BatchWriter bw, String rowKey) {
+        super(DataRateInjectionRandomDataOnTsScenario.class.getSimpleName(), bw);
+        this.rowKey = rowKey;
     }
 
     protected String createRandomKey(int index) {
         Random rnd = new Random();
         StringBuilder key = new StringBuilder();
-        for(int i = 0 ; i < 4; i++) {
+        key.append(this.rowKey);
+        for(int i = 0 ; i < 3; i++) {
             key.append((char) (rnd.nextInt(26) + 'a'));
         }
         key.append(index);
         return key.toString();
     }
 
-    protected Mutation createRandomMutation(int index) {
-        Mutation m = new Mutation(this.createRandomKey(index));
-        m.put("cf","cq", "my_value_" + index);
-        return m;
+    static class Opts extends ClientOnRequiredTable {
+        @Parameter(names = "--rowKey", required = true)
+        String rowKey = null;
     }
 
     public static void main(String[] args) throws Exception {
-        ClientOnRequiredTable opts = new ClientOnRequiredTable();
+        Opts opts = new Opts();
         BatchWriterOpts bwOpts = new BatchWriterOpts();
         opts.parseArgs(DataRateInjectionScenario.class.getName(), args, bwOpts);
         Connector connector = opts.getConnector();
 
         BatchWriter bw  = connector.createBatchWriter(opts.getTableName(), bwOpts.getBatchWriterConfig());
-        Scenario scenario = new DataRateInjectionRandomDataScenario(bw);
+        Scenario scenario = new DataRateInjectionRandomDataOnTsScenario(bw, opts.rowKey);
         scenario.run();
         scenario.finish();
     }
